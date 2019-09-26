9056403eb5e75c4c17f1311d2fcfcdd0fa2f33e2
Inihood1 <inihood@gmail.com>
2019-09-26 13:16:25 +0100

    Team-Geras-Solar-Calculator

---
+++ app/src/main/java/com/inihood/hng/Appliances.java
@@ -0,0 +1,13 @@
+package com.inihood.hng;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Appliances extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appliances);
+    }
+}
